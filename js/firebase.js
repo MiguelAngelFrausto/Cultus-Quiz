0071b8e87d70abdb6964fabcae3bda54378c2cf8
AndyCedillo <andycedillo@gmail.com>
2021-03-13 23:45:19 -0600

    usuarioFirebase

--- js/firebase.js
+++ js/firebase.js
@@ -4,14 +4,28 @@ const btnSignUp = document.querySelector("#formulario__register");
 
 btnSignUp.addEventListener('submit', (e) => {
 
-    const email = document.querySelector("#email").value;
-    const password = document.querySelector("#password").value;
+    const nombre = document.querySelector("#name").value;
+    const correo = document.querySelector("#email").value;
+    const contrasena = document.querySelector("#password").value;
     e.preventDefault();
 
-    console.log(email, password);
+    console.log(correo, contrasena);
 
-    auth.createUserWithEmailAndPassword(email, password).then(userCredential =>{
+    auth.createUserWithEmailAndPassword(correo, contrasena).then(userCredential =>{
         console.log('Registrado!');
+        
+        var uID = firebase.auth().currentUser;
+
+        var name, uid, mail;
+        if (uID != null){
+        //    mail = uID.email;
+            uid = uID.uid;
+
+            console.log(uid);
+        }
+
+        console.log(uID);
+
     });
 
 });

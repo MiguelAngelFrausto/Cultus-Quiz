3a5c6112007e793dceb6d2217697728a887edee4
AndyCedillo <andycedillo@gmail.com>
2021-03-14 00:30:57 -0600

    signIn?

--- js/firebase.js
+++ js/firebase.js
@@ -1,6 +1,7 @@
 //obtener elementos
 
 const btnSignUp = document.querySelector("#formulario__register");
+const btnSignIn = document.querySelector("#formulario__login");
 
 btnSignUp.addEventListener('submit', (e) => {
 
@@ -9,10 +10,10 @@ btnSignUp.addEventListener('submit', (e) => {
     const contrasena = document.querySelector("#password").value;
     e.preventDefault();
 
-    console.log(correo, contrasena);
+   // console.log(correo, contrasena);
 
     auth.createUserWithEmailAndPassword(correo, contrasena).then(userCredential =>{
-        console.log('Registrado!');
+       // console.log('Registrado!');
         
         var uID = firebase.auth().currentUser;
 
@@ -20,19 +21,66 @@ btnSignUp.addEventListener('submit', (e) => {
         if (uID != null){
         //    mail = uID.email;
             uid = uID.uid;
+            firebase.database().ref('Clientes/'+uid).set({
+                nombre: nombre,
+                correo: correo,
+                uID: uid
+            });
 
             console.log(uid);
         }
 
         console.log(uID);
+        iniciarSesion();
 
     });
+
 
 });
 
 
+btnSignIn.addEventListener('submit', (e) => {
+    const correo = document.querySelector("#mail").value;
+    const contrasena = document.querySelector("#pass").value;
+    e.preventDefault();
+
+
+    auth.signInWithEmailAndPassword(correo, contrasena).then(userCredential =>{
+
+        var uID = firebase.auth().currentUser;
+
+        if (uID != null){
+            
+        }
+
+    });
 
 
 
+})
+    
+
+// Formulario 
+function iniciarSesion(){
+    if(window.innerWidth > 850){
+        
+        formulario_register.style.display = "none";
+        contenedor_login_register.style.left = "10px";
+        formulario_login.style.display = "block"
+        caja_trasera_register.style.opacity = "1";
+        caja_trasera_login.style.opacity ="0";
+    
+    } else {
+
+        formulario_register.style.display = "none";
+        contenedor_login_register.style.left = "0px";
+        formulario_login.style.display = "block"
+        caja_trasera_register.style.display = "block";
+        caja_trasera_login.style.display ="none";
+
+    }
+    
+
+}
 
 

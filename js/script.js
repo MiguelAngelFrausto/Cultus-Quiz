27dd321219be24e53d95d5fe99897ea85cb7004a
AndyCedillo <andycedillo@gmail.com>
2021-03-15 12:29:49 -0600

    firebaseLogindone

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 document.getElementById("btn__registrarse").addEventListener("click", register);
 document.getElementById("btn__iniciar-sesion").addEventListener("click", iniciarSesion);
 
+//Obterner valores para firebase 
+const btnSignUp = document.querySelector("#formulario__register");
+const btnSignIn = document.querySelector("#formulario__login");
+
 window.addEventListener("resize", anchoPagina);
 
 //Declaracion de variable
@@ -78,3 +82,62 @@ function register(){
     }
 
 }
+
+
+///firebase auth login 
+btnSignUp.addEventListener('submit', (e) => {
+
+    const nombre = document.querySelector("#name").value;
+    const correo = document.querySelector("#email").value;
+    const contrasena = document.querySelector("#password").value;
+    e.preventDefault();
+
+   // console.log(correo, contrasena);
+
+    auth.createUserWithEmailAndPassword(correo, contrasena).then(userCredential =>{
+       // console.log('Registrado!');
+        
+        var uID = firebase.auth().currentUser;
+
+        var name, uid, mail;
+        if (uID != null){
+        //    mail = uID.email;
+            uid = uID.uid;
+            firebase.database().ref('Clientes/'+uid).set({
+                nombre: nombre,
+                correo: correo,
+                uID: uid
+            });
+
+            console.log(uid);
+        }
+
+        console.log(uID);
+        iniciarSesion();
+
+    });
+
+
+});
+
+
+btnSignIn.addEventListener('submit', (e) => {
+    const correo = document.querySelector("#mail").value;
+    const contrasena = document.querySelector("#pass").value;
+    e.preventDefault();
+
+
+    auth.signInWithEmailAndPassword(correo, contrasena).then(userCredential =>{
+
+        var uID = firebase.auth().currentUser;
+
+        if (uID != null){
+            return window.location.assign("/quiz.html");   
+        }
+
+    });
+
+
+
+})
+    
